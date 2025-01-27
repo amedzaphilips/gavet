d82fb4a31518b79c9f465a628e4cfab08ebcc439
Joshua <atsujoshua67@gmail.com>
2025-01-26 17:41:40 -0800

    prototype 1

--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -1,2 +1,8 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
-export default defineNuxtPlugin({})
+
+export default defineNuxtPlugin({
+  setup() {
+    // No specific setup needed for Bootstrap JS, it attaches to window automatically.
+  }
+});
+
